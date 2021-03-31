26e45f705e686697d63238fc2ccd247b0a37564e
Maruf Hasan <marufhasan212@gmail.com>
2021-03-31 21:20:19 +0600

    Initial commit

---
+++ app/src/main/java/com/example/arcadia/SignUp_activity.java
@@ -0,0 +1,14 @@
+package com.example.arcadia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_activity);
+    }
+}
